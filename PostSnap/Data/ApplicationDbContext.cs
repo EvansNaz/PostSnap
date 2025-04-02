d13f5d9792b87e7cd912dd363cb84cc83ed24f70
EvansNaz <baggelisnaz@gmail.com>
2025-04-03 01:02:21 +0300

    v1 Database

--- PostSnap/Data/ApplicationDbContext.cs
+++ PostSnap/Data/ApplicationDbContext.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PostSnap.Models;
+using System.Reflection.Emit;
 
 namespace PostSnap.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Post> Posts { get; set; }
+        //Next is Comments
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Ensure IdentityDbContext configurations are set
+
+            // Apply soft delete globally for Posts
+            modelBuilder.Entity<Post>().HasQueryFilter(p => !p.IsDeleted); // Exclude deleted posts by default
+
+            // Apply soft delete globally for Comments
+            // modelBuilder.Entity<Comment>().HasQueryFilter(c => !c.IsDeleted);
+        }
+
+
     }
 }
---
+++ PostSnap/Models/Post.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostSnap.Models
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Auto increment
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public PostStatus Status { get; set; } = PostStatus.Published;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastModifiedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        //Foreign key linking to Identity User
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual IdentityUser User { get; set; }//lazy loading
+    }
+
+    public enum PostStatus
+    {
+        Draft,
+        Published,
+        Deleted
+    }
+}
